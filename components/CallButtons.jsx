dda208766b913a5c5f625d5fe8cad9cb9916df12
krinedalr-stack <krinedalr@gmail.com>
2025-12-01 20:50:15 +0000

    Update CallButtons.jsx

--- components/CallButtons.jsx
+++ components/CallButtons.jsx
@@ -1,13 +1,13 @@
 // components/CallButtons.jsx
 export default function CallButtons() {
   return (
-    <div className="btn-row">
-      <a className="btn btn-primary" href="tel:0831762475">
+    <div className="hero-actions">
+      <a href="tel:0831762475" className="btn btn-primary">
         Call 24/7 Storm Line
       </a>
       <a
+        href="mailto:krinedalremail@gmail.com?subject=Quote%20request"
         className="btn btn-outline"
-        href="mailto:hello.krinedalr@gmail.com?subject=Quote%20request%20from%20website"
       >
         Request a quote by email
       </a>
